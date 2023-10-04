d1e22ad0504329d5fe99f10599497f1b69761a20
Lalitha <lalijbharathi@gmail.com>
2023-10-04 19:01:11 +0530

    adding to problems

--- strings/practice_problems/stud_mark.py
+++ strings/practice_problems/stud_mark.py
@@ -4,16 +4,19 @@
 # Pass mark is 50.
 # Print a new list with all the students with pass marks along with their mark in the format name:mark.
 # Also print the number of students who failed.
-student_names = ['lali','mithu','theeru','jaya']
-student_marks = [45,60,75,34,]
-pass_mark = 50
-pass_list =[]
-fail_list =[]
-for i in range(len(student_names)):
-    if student_marks[i] >= pass_mark:
-        pass_list.append(student_names[i]+ ":"+ str(student_marks[i]))
+student_names = ['lali','mithu','theeru','jaya'] # create a list for student name
+student_marks = [45,60,75,34,]# create a list for student marks
+pass_mark = 50 #initialise pass mark as 50
+pass_list =[] # empty array for pass list
+fail_list =[] # empty array for fail list
+for i in range(len(student_names)): 
+    if student_marks[i] >= pass_mark:# checking for pass mark
+        pass_list.append(student_names[i]+ ":"+ str(student_marks[i]))# add the passed students to the pass list
     else :
-        fail_list.append(student_names[i])
-print(pass_list)
+        fail_list.append(student_names[i])# add the failed students to the list
+print(pass_list) 
 print(f"the number of students failed:{len(fail_list)}")
 
+# output:
+# ['mithu:60', 'theeru:75']
+# the number of students failed:2
