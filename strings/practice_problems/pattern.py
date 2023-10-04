e0862a4e589a71aeb0cb7349e78e4462d3241801
Lalitha <lalijbharathi@gmail.com>
2023-10-04 18:56:22 +0530

    adding pattern to problems

--- strings/practice_problems/pattern.py
+++ strings/practice_problems/pattern.py
@@ -7,9 +7,17 @@
 # abacabadabacabaeabacabadabacaba
 
 # look at the output and find the pattern. Hint - add next letter in the alphabet in each row
-wrapper_str = ""
-for value in range(97, ord('g') + 1):
-   wrapper_str = wrapper_str + chr(value) + wrapper_str
+#initialising the wrapper str as empty where it holds the current string value
+wrapper_str = "" 
+for value in range(97, ord('g') + 1):# using an ascii value for alphabets
+   wrapper_str = wrapper_str + chr(value) + wrapper_str 
    print(wrapper_str)
 
-   
+# output:
+# a
+# aba
+# abacaba
+# abacabadabacaba
+# abacabadabacabaeabacabadabacaba
+# abacabadabacabaeabacabadabacabafabacabadabacabaeabacabadabacaba
+# abacabadabacabaeabacabadabacabafabacabadabacabaeabacabadabacabagabacabadabacabaeabacabadabacabafabacabadabacabaeabacabadabacaba
