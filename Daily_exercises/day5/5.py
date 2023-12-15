c8c14176620e4c5db70e0b7e85fdfbd45c247058
lalitha-r <44593556+lalitha-r@users.noreply.github.com>
2023-12-15 17:37:22 +0530

    delete 5.py

--- Daily_exercises/day5/5.py
+++ Daily_exercises/day5/5.py
@@ -1,8 +1,1 @@
-def initialize_theater(rows, seats_per_row):
-    seating = {chr(65 + i): [False] * seats_per_row for i in range(rows)}
-    available_seats = rows * seats_per_row
-    return seating, available_seats
 
-
-output = initialize_theater(5, 10)
-print(output)
